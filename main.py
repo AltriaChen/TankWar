46af9268c6dacf06c7d52266cca31ed3dd59b76e
AltriaChen <mwchennju@foxmail.com>
2021-02-25 15:39:50 +0800

    TankWar

---
+++ main.py
@@ -0,0 +1,11 @@
+# Thanks to  Zack
+# Time  2021/02/04
+# Development tool  Pycharm 2020
+# Environment  Python 3.8.3
+# Module  pygame
+
+from tank_war import TankWar
+
+if __name__ == '__main__':
+    tankWar = TankWar()
+    tankWar.run_game()
